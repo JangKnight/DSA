d59c8ed7b6fb548df5add7941b72df0b5a53fff6
Professor Knight <profdevent@gmail.com>
2026-01-09 09:18:18 -0500

    add best solution: ips_between

--- ips_between/ips_between.py
+++ ips_between/ips_between.py
@@ -1,22 +1,35 @@
+from ipaddress import ip_address
+
 def ips_between(start, end):
-    start = start.split('.')
-    end = end.split('.')
-    dist = 0
-    if int(end[0]) > int(start[0]):
-        dist += abs(int(end[0]) - int(start[0])) * 16777216
-    if int(end[1]) > int(start[1]):
-        dist += abs(int(end[1]) - int(start[1])) * 65536
-    if int(end[2]) > int(start[2]):
-        dist += abs(int(end[2]) - int(start[2])) * 256
-    if int(end[3]) > int(start[3]):
-        dist += abs(int(end[3]) - int(start[3]))
-    if int(end[0]) < int(start[0]):
-        dist -= abs(int(end[0]) - int(start[0])) * 16777216
-    if int(end[1]) < int(start[1]):
-        dist -= abs(int(end[1]) - int(start[1])) * 65536
-    if int(end[2]) < int(start[2]):
-        dist -= abs(int(end[2]) - int(start[2])) * 256
-    if int(end[3]) < int(start[3]):
-        dist -= abs(int(end[3]) - int(start[3]))
+    return int(ip_address(end)) - int(ip_address(start))
+
+=====================================OLD CODE: DO NOT USE OR DELETE
+
+# def ips_between(start, end):
+#     a = sum([int(e)*256**(3-i) for i, e in enumerate(start.split('.'))])
+#     b = sum([int(e)*256**(3-i) for i, e in enumerate(end.split('.'))])
+#     return abs(a-b)
+
+======================================
+# def ips_between(start, end):
+#     start = start.split('.')
+#     end = end.split('.')
+#     dist = 0
+#     if int(end[0]) > int(start[0]):
+#         dist += abs(int(end[0]) - int(start[0])) * 16777216
+#     if int(end[1]) > int(start[1]):
+#         dist += abs(int(end[1]) - int(start[1])) * 65536
+#     if int(end[2]) > int(start[2]):
+#         dist += abs(int(end[2]) - int(start[2])) * 256
+#     if int(end[3]) > int(start[3]):
+#         dist += abs(int(end[3]) - int(start[3]))
+#     if int(end[0]) < int(start[0]):
+#         dist -= abs(int(end[0]) - int(start[0])) * 16777216
+#     if int(end[1]) < int(start[1]):
+#         dist -= abs(int(end[1]) - int(start[1])) * 65536
+#     if int(end[2]) < int(start[2]):
+#         dist -= abs(int(end[2]) - int(start[2])) * 256
+#     if int(end[3]) < int(start[3]):
+#         dist -= abs(int(end[3]) - int(start[3]))
         
-    return int(dist)
+#     return int(dist)
